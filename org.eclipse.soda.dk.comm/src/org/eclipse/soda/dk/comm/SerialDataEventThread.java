e3a3175240e3f0d9f86ab41a45ef1d128d2058f9
dlavin <>
2008-01-04 20:22:23 +0000

    *** empty log message ***

--- org.eclipse.soda.dk.comm/src/org/eclipse/soda/dk/comm/SerialDataEventThread.java
+++ org.eclipse.soda.dk.comm/src/org/eclipse/soda/dk/comm/SerialDataEventThread.java
@@ -1,7 +1,7 @@
 package org.eclipse.soda.dk.comm;
 
 /*************************************************************************
- * Copyright (c) 2007 IBM.                                               *
+ * Copyright (c) 2007, 2008 IBM.                                         *
  * All rights reserved. This program and the accompanying materials      *
  * are made available under the terms of the Eclipse Public License v1.0 *
  * which accompanies this distribution, and is available at              *
