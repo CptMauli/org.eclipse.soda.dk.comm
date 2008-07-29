aa3f17e32950b1345e55375f834849b61c75933e
dlavin <>
2008-07-29 20:14:17 +0000

    *** empty log message ***

--- org.eclipse.soda.dk.comm/src/org/eclipse/soda/dk/comm/NSCommDriver.java
+++ org.eclipse.soda.dk.comm/src/org/eclipse/soda/dk/comm/NSCommDriver.java
@@ -1,9 +1,15 @@
 package org.eclipse.soda.dk.comm;
 
-/**************************************************************************************************************************************************************************************************************************************************************
- * Copyright (c) 1999, 2008 IBM. * All rights reserved. This program and the accompanying materials * are made available under the terms of the Eclipse Public License v1.0 * which accompanies this distribution, and is available at *
- * http://www.eclipse.org/legal/epl-v10.html * * Contributors: * IBM - initial API and implementation *
- *************************************************************************************************************************************************************************************************************************************************************/
+/*************************************************************************
+ * Copyright (c) 1999, 2008 IBM.                                         *
+ * All rights reserved. This program and the accompanying materials      *
+ * are made available under the terms of the Eclipse Public License v1.0 *
+ * which accompanies this distribution, and is available at              *
+ * http://www.eclipse.org/legal/epl-v10.html                             *
+ *                                                                       *
+ * Contributors:                                                         *
+ *     IBM - initial API and implementation                              *
+ ************************************************************************/
 import java.io.IOException;
 
 import javax.comm.CommDriver;
--- org.eclipse.soda.dk.comm/src/org/eclipse/soda/dk/comm/internal/Library.java
+++ org.eclipse.soda.dk.comm/src/org/eclipse/soda/dk/comm/internal/Library.java
@@ -1,9 +1,15 @@
 package org.eclipse.soda.dk.comm.internal;
 
-/**************************************************************************************************************************************************************************************************************************************************************
- * Copyright (c) 2007, 2008 IBM. * All rights reserved. This program and the accompanying materials * are made available under the terms of the Eclipse Public License v1.0 * which accompanies this distribution, and is available at *
- * http://www.eclipse.org/legal/epl-v10.html * * Contributors: * IBM - initial API and implementation *
- *************************************************************************************************************************************************************************************************************************************************************/
+/*************************************************************************
+ * Copyright (c) 2007, 2008 IBM.                                         *
+ * All rights reserved. This program and the accompanying materials      *
+ * are made available under the terms of the Eclipse Public License v1.0 *
+ * which accompanies this distribution, and is available at              *
+ * http://www.eclipse.org/legal/epl-v10.html                             *
+ *                                                                       *
+ * Contributors:                                                         *
+ *     IBM - initial API and implementation                              *
+ ************************************************************************/
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
